01473f1e0ad84931af184853f323a11a24b10bf1
Ahana Nandi <ahananondi6@gmail.com>
2026-03-15 16:44:06 +0600

    layout as root

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './Routes/Routes.jsx'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <h2>Hello World</h2>
-  }
-])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
